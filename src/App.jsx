908ed39c6c4b87190a9fa61700a382c9e40847f5
Maxime-Monjal <maxime.monjal@gmail.com>
2021-01-16 10:47:09 +0100

    test deploy

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/views/Home/Home";
+import Product from "./components/views/Product";
+import Header from "./components/commons/Header/Header";
+import Footer from "./components/commons/Footer/Footer";
+import Cart from "./components/views/Cart/Cart";
+import Page404 from "./components/views/Page404/Page404";
+
+function App() {
+  return (
+    <Router>
+      <Helmet>
+        <title>Magic Pills</title>
+        <meta
+          name="description"
+          content="This is a site to become a super hero for a brief moment"
+        />
+      </Helmet>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/produits" component={Home} />
+        <Route exact path="/produits/:categorie" component={Home} />
+        <Route path="/product/:power" component={Product} />
+        <Route path="/panier" component={Cart} />
+        <Route path="/:all" component={Page404} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
